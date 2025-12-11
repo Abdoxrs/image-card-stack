13b17a3bbe9ef00a7c4f666806898ac7962aa582
Abdelrhman <abdoxrs223@gmail.com>
2025-12-12 00:01:09 +0200

    Adding reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { ImageCardStack } from "./components/features/ImageStack";
-
+import { ImageStack } from "./components/features/ImageStack";
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <ImageCardStack />
+    <div className="min-h-screen flex flex-col items-center justify-center p-4 text-cyan-600">
+      <h1 className="text-3xl font-extrabold text-center mb-15">Image-Card Slider</h1>
+      <div><ImageStack/></div>
     </div>
   );
 }
